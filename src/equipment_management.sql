d10d33a77102cd7b7856c5e96be72182b21c6c69
Leah505 <leah.clarke@ada.ac.uk>
2025-01-31 18:09:58 +0000

    task three

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -6,11 +6,25 @@
 
 -- Equipment Management Queries
 
--- 1. Find equipment due for maintenance
--- TODO: Write a query to find equipment due for maintenance
+-- 3.1. Find equipment due for maintenance in the next 30 days
+SELECT 
+    equipment_id, 
+    name, 
+    next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date BETWEEN DATE('now') AND DATE('now', '+30 days')
+ORDER BY next_maintenance_date;
 
--- 2. Count equipment types in stock
--- TODO: Write a query to count equipment types in stock
+-- 3.2. Count equipment types in stock
+SELECT 
+    type AS equipment_type, 
+    COUNT(*) AS count
+FROM equipment
+GROUP BY type;
 
--- 3. Calculate average age of equipment by type (in days)
--- TODO: Write a query to calculate average age of equipment by type (in days)
+-- 3.3. Calculate average age of equipment by type (in days)
+SELECT 
+    type AS equipment_type, 
+    AVG(JULIANDAY('now') - JULIANDAY(purchase_date)) AS avg_age_days
+FROM equipment
+GROUP BY type;
